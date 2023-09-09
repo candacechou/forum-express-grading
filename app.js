5125277b6670662f737b97ba8429740b77dda6c6
candacechou <candace01250125@gmail.com>
2023-09-10 00:03:06 +0800

    fix: top restaurant with no description

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ const { pages, apis } = require('./routes')
 const passport = require('./config/passport')
 // const { getUser } = require('./helpers/auth-helpers')
 
-
 const handlebarsHelpers = require('./helpers/handlebars-helpers')
 const app = express()
 const port = process.env.PORT || 3000
